01889ec14f6e992a51a57ed3b1b2e89bba309935
Sinan ACAR <sinanacar14654@gmail.com>
2017-07-17 23:28:50 +0300

    Add files via upload

---
+++ Tv-programı/Tv-programı/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Tv_programı
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void axWindowsMediaPlayer1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            axWindowsMediaPlayer1.URL = "http://31.210.157.109/360tv/360tvlive2/playlist.m3u8 ";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            axWindowsMediaPlayer1.URL = "http://media.netd.com.tr/S2/HLS_LIVE/cnn_turk/index.m3u8";
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            axWindowsMediaPlayer1.URL = " http://yayin3.canlitv.com:1935/canlitv/disney/playlist.m3u8 ";
+        }
+
+      
+        private void button4_Click(object sender, EventArgs e)
+        {
+            axWindowsMediaPlayer1.URL = " http://live.netd.com.tr/S1/HLS_LIVE/dreamturktv/index.m3u8";
+        }
+    }
+}
